2cef76f637cd249ddf61b6dfaa9b55817008bc81
Ankita Patil <ankitapatil010407@gmail.com>
2026-05-14 13:53:36 +0530

    gallary section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,27 @@ const navLinks = document.querySelector('.nav-links');
 mobileToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+}
